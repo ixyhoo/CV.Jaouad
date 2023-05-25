a2640cc71153cd3d17a08f1ef1a61bf0f691dc4f
ixyhoo <ixyhoo@outlook.fr>
2023-05-25 21:41:29 +0200

    JsPDF error is not resolved

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
-    document.getElementById('download').addEventListener('click', function () {
-        var main = document.querySelector('body');
-        html2canvas(main).then(canvas => {
-            let img = canvas.toDataURL('image/png');
-            let doc = new jsPDF();
-            doc.addImage(img, 'JPEG', 0, 0);
-            doc.save('cv_jaouad.pdf');
-        });
+document.getElementById('download').addEventListener('click', function () {
+    let main = document.querySelector('body');
+    html2canvas(main).then(canvas => {
+        let img = canvas.toDataURL('image/png');
+        let doc = new jsPDF.jsPDF(); // Pour jsPDF version 2.0.0 et plus tard
+        doc.addImage(img, 'JPEG', 0, 0);
+        doc.save('cv_jaouad.pdf');
     });
-
+});
+ 
